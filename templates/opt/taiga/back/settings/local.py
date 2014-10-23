761c6c82fd0228399453b64c074b183110212398
JScott <jvscott@gmail.com>
2014-10-23 21:30:49 +0000

    taiga - fixed email

--- templates/opt/taiga/back/settings/local.py
+++ templates/opt/taiga/back/settings/local.py
@@ -12,12 +12,12 @@ PUBLIC_REGISTER_ENABLED = True
 DEFAULT_FROM_EMAIL = "{{ taiga_from_email_address }}"
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
-#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#EMAIL_USE_TLS = False
-#EMAIL_HOST = "localhost"
-#EMAIL_HOST_USER = ""
-#EMAIL_HOST_PASSWORD = ""
-#EMAIL_PORT = 25
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = "{{ taiga_email_host }}"
+EMAIL_HOST_USER = "{{ taiga_aws_access_key_id }}"
+EMAIL_HOST_PASSWORD = "{{ taiga_aws_secret_access_key }}"
+EMAIL_PORT = '"{{ taiga_email_port }}"'
 
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = (
     "rest_framework.renderers.JSONRenderer",
